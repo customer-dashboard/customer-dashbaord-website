280c3b79aca71fc05be5691762ceeffc6a44be7c
darshan tailor <customerdashboardpro@gmail.com>
2024-01-19 12:11:58 +0530

    page redirect 1/19/2024 12:11 AM

--- app/routes/pages.faq.jsx
+++ app/routes/pages.faq.jsx
@@ -1,21 +1,10 @@
-// src/routes/oldUrl.js
+// src/routes/pages/faq.js
 
 import { redirect } from '@remix-run/node';
 
-export async function loader({ request }) {
-  const oldUrlPrefix = "https://customerdashboard.pro/pages/faq";
-  const newUrl = "https://customerdashboard.pro/faqs";
-
-  // Requested URL ke prefix ko check karein
-  if (request.url.startsWith(oldUrlPrefix)) {
-    // Purane URL ka prefix hata kar naya URL banaayein
-    const remainingPath = request.url.slice(oldUrlPrefix.length);
-    const newRedirectUrl = `${newUrl}${remainingPath}`;
-
-    // Sirf redirect karein, kuch aur na dikhaayein
-    return redirect(newRedirectUrl);
-  }
-
-  // Agar requested URL purane URL ke saath match nahi karta, to normal response de
-  return { status: 200, body: "Remix Run mein swagat hai!" };
+export async function loader() {
+  const newUrl = "/faqs";
+  
+  // Sirf redirect karein, kuch aur na dikhaayein
+  return redirect(newUrl);
 }
